a500ab4ca99fd8f6a4bd9b56354ddf3bbb65437b
Andrii Osipov <aosipov@anaconda.com>
2025-03-28 01:45:22 -0700

    removed reg setup

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -20,49 +20,29 @@ set "ODBCINI=%CONDA_PREFIX%\etc\odbc.ini"
 :: Set variable for driver location
 set "MSODBCSQL%MAJOR_VERSION%_DRIVER_PATH=%CONDA_PREFIX%\Library\bin\msodbcsql%MAJOR_VERSION%.dll"
 
-:: Backup registry state before modification
-set "REG_BACKUP_DIR=%CONDA_PREFIX%\etc\odbc_reg_backup"
-if not exist "%REG_BACKUP_DIR%" mkdir "%REG_BACKUP_DIR%"
-
-:: Check if driver already exists in registry and back it up
-reg query "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" /v "ODBC Driver %MAJOR_VERSION% for SQL Server" >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
-    echo Backing up existing registry entries...
-    reg export "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" "%REG_BACKUP_DIR%\driver_entry.reg" /y >nul 2>&1
-    set "_CONDA_ODBC_DRIVER_EXISTED=1"
-) else (
-    set "_CONDA_ODBC_DRIVER_EXISTED=0"
-)
-
-:: Also check and backup the driver list entry
-reg query "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
-    reg export "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" "%REG_BACKUP_DIR%\drivers_list.reg" /y >nul 2>&1
-)
-
-:: Register in Windows registry
-set "FULL_PATH=%CONDA_PREFIX:\=\\%"
-set "FULL_DRIVER_PATH=%FULL_PATH%\Library\bin\msodbcsql%MAJOR_VERSION%.dll"
-
-:: pyodbc (and the native Windows ODBC Driver Manager) by default only looks in HKLM:
-:: pyodbc.drivers() # reads HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers
-:: HKCU is ignored.
-:: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-:: Action	                                Where to write?	        Why?
-:: Official driver installation	            HKLM	                The driver is available system-wide
-:: Temporary registration for the user	    HKCU	                No admin rights needed, but pyodbc won't see it
-:: pyodbc must detect the driver	        HKLM	                pyodbc looks there
-:: ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" /v "ODBC Driver %MAJOR_VERSION% for SQL Server" /t REG_SZ /d "Installed" /f >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
-    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v Driver /t REG_SZ /d "%FULL_DRIVER_PATH%" /f >nul 2>&1
-    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v Description /t REG_SZ /d "Microsoft ODBC Driver %MAJOR_VERSION% for SQL Server" /f >nul 2>&1
-    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v Threading /t REG_SZ /d "1" /f >nul 2>&1
-    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v UsageCount /t REG_DWORD /d 1 /f >nul 2>&1
-    echo =^> Driver registered in Windows registry (HKLM)
-) else (
-    echo =^> WARNING: Could not register driver in Windows registry - pyodbc may not see the driver
-    echo =^> Driver will still be available via environment configuration
-)
+echo ====================================================================
+echo IMPORTANT: Manual ODBC Driver Registration Required
+echo ====================================================================
+echo.
+echo The ODBC Driver %MAJOR_VERSION% for SQL Server needs to be registered
+echo in the Windows registry for applications like pyodbc to detect it.
+echo.
+echo Please follow these steps to register the driver manually:
+echo.
+echo 1. Open an Administrator Command Prompt
+echo 2. Run the following commands:
+echo.
+echo    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" /v "ODBC Driver %MAJOR_VERSION% for SQL Server" /t REG_SZ /d "Installed" /f
+echo    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v Driver /t REG_SZ /d "%CONDA_PREFIX%\Library\bin\msodbcsql%MAJOR_VERSION%.dll" /f
+echo    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v Description /t REG_SZ /d "Microsoft ODBC Driver %MAJOR_VERSION% for SQL Server" /f
+echo    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v Threading /t REG_SZ /d "1" /f
+echo    reg add "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver %MAJOR_VERSION% for SQL Server" /v UsageCount /t REG_DWORD /d 1 /f
+echo.
+echo Alternatively, you can use the odbcconf utility:
+echo    odbcconf /a {CONFIGDRIVER "ODBC Driver %MAJOR_VERSION% for SQL Server" INSTALL "Microsoft ODBC Driver %MAJOR_VERSION% for SQL Server" "%CONDA_PREFIX%\Library\bin\msodbcsql%MAJOR_VERSION%.dll"}
+echo.
+echo NOTE: Environment variables for ODBC configuration have been set,
+echo but pyodbc and other applications may still require HKLM registry registration.
+echo ====================================================================
 
 echo =^> Environment variables for Microsoft ODBC Driver %MAJOR_VERSION% configured
--- recipe/deactivate.bat
+++ recipe/deactivate.bat
@@ -1,29 +1,5 @@
 @echo off
 
-:: Check if we need to restore or delete registry entries
-set "REG_BACKUP_DIR=%CONDA_PREFIX%\etc\odbc_reg_backup"
-
-if defined _CONDA_ODBC_DRIVER_EXISTED (
-    if %_CONDA_ODBC_DRIVER_EXISTED% EQU 1 (
-        echo Restoring original registry entries...
-        if exist "%REG_BACKUP_DIR%\driver_entry.reg" (
-            reg import "%REG_BACKUP_DIR%\driver_entry.reg" >nul 2>&1
-        )
-        if exist "%REG_BACKUP_DIR%\drivers_list.reg" (
-            reg import "%REG_BACKUP_DIR%\drivers_list.reg" >nul 2>&1
-        )
-    ) else (
-        echo Removing added registry entries...
-        reg delete "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver 18 for SQL Server" /f >nul 2>&1
-        reg delete "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" /v "ODBC Driver 18 for SQL Server" /f >nul 2>&1
-    )
-    set "_CONDA_ODBC_DRIVER_EXISTED="
-) else (
-    echo WARNING: Registry state could not be determined, performing standard cleanup...
-    reg delete "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Driver 18 for SQL Server" /f >nul 2>&1
-    reg delete "HKLM\SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers" /v "ODBC Driver 18 for SQL Server" /f >nul 2>&1
-)
-
 :: Restore previous variable values (if they existed)
 if defined _CONDA_BACKUP_ODBCSYSINI (
     set "ODBCSYSINI=%_CONDA_BACKUP_ODBCSYSINI%"
